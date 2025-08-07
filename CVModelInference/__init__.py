e2a7937ac77e1d028ee616b5af075d7545750c10
Abhinav Rai <1229403+esmeagol@users.noreply.github.com>
2025-08-06 20:44:57 -0700

    Remove model conversion scripts

--- CVModelInference/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-CVModelInference - Deep Learning for Cue Sports Analysis
-
-This module contains deep learning models and inference pipelines for analyzing
-cue sports using modern computer vision techniques.
-"""
-
-__version__ = "0.1.0"
